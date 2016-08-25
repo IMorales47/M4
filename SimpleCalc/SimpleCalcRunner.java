4b5e98886253d7e248ede136a86c34cce95eacc1
unknown <ifajardo@WS-I3-26.WS-ACADBUS.EDU>
2016-08-25 08:49:07 -0700

    lol

---
+++ SimpleCalc/SimpleCalcRunner.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class SimpleCalcRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SimpleCalcRunner
+{
+   public static void main (String[] args){
+      int n1 = 1123;
+      int n2 = 567;
+      simpleCalc sc = new SimpleCalc(n1, n2);
+      System.out.prinln("Numbers: " + n1 + "," + n2);
+      System.out.prinln("The sum is: " + sc.add());
+      System.out.prinln(
+      System.out.prinln(
+      System.out.prinln(
+      System.out.prinln(
+    
+    
+    
+    
+    
+    
+    
+   }
+}
